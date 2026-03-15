33f2c73f78a4d91f48741626028507f178300c15
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-03-15 18:51:16 +0100

    Update place.py

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -3,7 +3,7 @@ from app.models.base import BaseModel
 
 place_amenity = db.Table('place_amenity',
     db.Column('place_id', db.String(36), db.ForeignKey('places.id', ondelete='CASCADE'), primary_key=True),
-    db.Column('amenity_id', db.String(36), db.ForeignKey('amenities.id', ondelete='CASCADE'), primary_key=True)
+    db.Column('amenity_id', db.String(36), db.ForeignKey('Amenity.id', ondelete='CASCADE'), primary_key=True)
 )
 
 class Place(BaseModel):
