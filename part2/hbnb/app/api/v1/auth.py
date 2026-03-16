781b9b3f24f37bdca8657ca1555f46426a3f7339
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-03-16 17:33:02 +0100

    Update auth.py

--- part2/hbnb/app/api/v1/auth.py
+++ part2/hbnb/app/api/v1/auth.py
@@ -5,7 +5,6 @@ from flask_bcrypt import check_password_hash
 
 api = Namespace('auth', description='Opérations d\'authentification')
 
-# Modèle pour Swagger
 login_model = api.model('Login', {
     'email': fields.String(required=True, description='Email de l\'utilisateur'),
     'password': fields.String(required=True, description='Mot de passe de l\'utilisateur')
@@ -19,21 +18,20 @@ class LoginResource(Resource):
         data = api.payload
         user = facade.get_user_by_email(data['email'])
 
-        # --- ZONE DE DEBUG ---
         if user:
             print(f"DEBUG: Mot de passe en base -> {user.password}")
-            # On vérifie si Bcrypt valide la comparaison
             is_valid = check_password_hash(user.password, data['password'])
             print(f"DEBUG: Comparaison Bcrypt -> {is_valid}")
         else:
             print("DEBUG: Utilisateur non trouvé dans la base")
-        # ---------------------
 
         if user and check_password_hash(user.password, data['password']):
-            # On crée le token avec l'email et le statut admin
+            
             access_token = create_access_token(
-                identity={'email': user.email, 'is_admin': user.is_admin}
+                identity=str(user.id), 
+                additional_claims={'is_admin': user.is_admin}
             )
+            
             return {'access_token': access_token}, 200
         
         return {'msg': 'Email ou mot de passe invalide'}, 401
